1a5e899082f86bf076e7f70a1eccd7d84123f634
Amir Mohammad Karimi <35371206+AMK9978@users.noreply.github.com>
2019-07-12 06:57:21 +0430

    Kattis solutions

---
+++ Kattis/Flying saftey/Main7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = scanner.nextInt();
+            int m = scanner.nextInt();
+            for (int j = 0; j < m; j++) {
+                int a = scanner.nextInt();
+                int b = scanner.nextInt();
+            }
+            System.out.println(n - 1);
+        }
+    }
+}
