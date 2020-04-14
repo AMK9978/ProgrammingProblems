0cf04bd9e95c8c2064790d875b5329fb215a3b48
Amir Mohammad Karimi <35371206+AMK9978@users.noreply.github.com>
2020-04-14 21:41:31 +0430

    Update Main2.java
    It's mine!

--- Kattis/Muzicary/Main2.java
+++ Kattis/Muzicary/Main2.java
@@ -1,58 +1,75 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main2 {
+public class Main3 {
+    private static int T, N;
+    private static int[] weigh;
+    private static X[][] memo;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int T = scanner.nextInt();
-        int N = scanner.nextInt();
-        int arr[] = new int[N];
-        int memo[][] = new int[N + 1][T + 1];
+        T = scanner.nextInt();
+        N = scanner.nextInt();
+        weigh = new int[N + 1];
+        memo = new X[N + 1][T + 1];
         for (int i = 0; i < N; i++) {
-            arr[i] = scanner.nextInt();
+            weigh[i] = scanner.nextInt();
         }
-        // We solve it like a knapsack problem, but assume any empty space is negative and
-        // we're gonna fill it as much as possible,thus in best position,
-        // memo[N][T] will be zero,weight of any item is length of rest
-        // value of any item is again length,so any item val/weight = 1 and we're only want to fill it
-        for (int i = 0; i <= T; i++) {
-            memo[0][i] = -i;
-        }
-        int mark[][] = new int[N + 1][T + 1];
-        for (int i = 1; i <= N; i++) {
-            for (int j = 0; j <= T; j++) {
-                if (arr[i - 1] <= j && memo[i - 1][j] - 1 < memo[i][j - arr[i - 1]]) {
-                    memo[i][j] = memo[i - 1][j - arr[i - 1]];
-                    mark[i][j] = 1;
-//                    System.out.println(i + ",J:" + j);
-                }else{
-                    memo[i][j] = memo[i - 1][j] - 1;
-                }
-            }
-        }
-        int j = T;
-        int belong[] = new int[N + 1];
-        /*Problem has two lines of rest
-            we have to calculate which one of musicians can rest in the first line,
-            rest belong to second line:
-        */
-        for (int i = N; j > 0 && i > 0 ; i--) {
-            if (mark[i][j] == 1){
-                j -= arr[i - 1];
-                belong[i - 1] = 1;
-            }
-        }
-        //We're sure any musician that doesn't belong to first line belong to second:
-        int one = 0 , two = 0;
+        X res = func(0, T, new ArrayList<>());
+//        System.out.println(res.arrayList);
+        int one = 0, two = 0;
         for (int i = 0; i < N; i++) {
-            if (belong[i] == 1){
+            if (res.arrayList != null && res.arrayList.contains(i)) {
                 System.out.print(one + " ");
-                one += arr[i];
-            }else{
+                one += weigh[i];
+            } else {
                 System.out.print(two + " ");
-                two += arr[i];
+                two += weigh[i];
             }
+        }
+    }
+
+    private static X func(int id, int rem, ArrayList<Integer> arrayList) {
+        if (rem < 0) {
+            return new X(arrayList, Integer.MIN_VALUE);
+        }
+        if (id == N) {
+            return new X(arrayList, -rem);
+        }
+        if (weigh[id] > rem) {
+            return func(id + 1, rem, arrayList);
+        }
+        if (memo[id][rem] != null) {
+            return memo[id][rem];
+        }
+        ArrayList<Integer> arrayList1 = new ArrayList<>();
+        arrayList1.add(id);
+        arrayList1.addAll(arrayList);
+        X x1 = func(id + 1, rem - weigh[id], arrayList1);
+        X x2 = func(id + 1, rem, arrayList);
+        if (x1.res >= x2.res) {
+            memo[id][rem] = x1;
+        } else {
+            memo[id][rem] = x2;
+        }
+        return memo[id][rem];
+    }
+
+    private static class X {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        int res = 0;
+
+        X(ArrayList<Integer> arrayList, int res) {
+            this.arrayList = arrayList;
+            this.res = res;
         }
 
+        @Override
+        public String toString() {
+            return "X{" +
+                    "arrayList=" + arrayList +
+                    ", res=" + res +
+                    '}';
+        }
     }
 }
