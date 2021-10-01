d77e629637435f1e5891e0cfe95b5809324ba9ff
zijing.niuace <zijing.niuace@gmail.com>
2021-10-01 22:34:19 +0800

    [Kattis] Solving For Carrots

---
+++ Kattis/Solving For Carrot/main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class main {
+
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int N = sc.nextInt();
+      int P = sc.nextInt();
+      String [] str = new String[N];
+      for (int i = 0 ; i<N ; i++)
+          str[i]=sc.nextLine();
+        System.out.println(P);
+    }
+    
+}
